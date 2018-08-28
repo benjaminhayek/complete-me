df47edff8c490567a462743a9779e0a83060e28c
bhayek12@gmail.com <Ben@Bens-MacBook-Pro-2.local>
2018-08-28 15:42:50 -0600

    Insert tests

--- test/node-test.js
+++ test/node-test.js
@@ -10,17 +10,19 @@ describe('NODE', () => {
 
  it('should exist now', () => {
   expect(node).to.exist;
- })
+ });
 
- it('should take a letter as an argument and assign it to a property', () => {
-  expect(node.letter).to.equal('p');
- })
+ // it('should take a letter as an argument and assign it to a property', () => {
+ //  expect(node.letter).to.equal('p');
+ // });
 
  it('should default the end to false', () => {
   expect(node.end).to.equal(false);
- })
+ });
 
- it('should default children to false', () => {
-  expect(node.children).to.equal(null);
- })
-})
+ it('should default children to object', () => {
+  expect(node.children).to.deep.equal({});
+ });
+
+});
+
--- test/trie-test.js
+++ test/trie-test.js
@@ -1,5 +1,6 @@
 import { expect } from'chai';
 import Trie from '../lib/trie';
+import Node from '../lib/Node';
 
 describe('TRIE', () => {
   let trie;
@@ -16,14 +17,21 @@ describe('TRIE', () => {
     expect(trie.totalWords).to.equal(0)
   });
 
-  it('should default root to null', () => {
-    expect(trie.root).to.equal(null)
+  it('should default root to empty object', () => {
+    expect(trie.root.children).to.deep.equal({})
   });
 
   it('should increase totalWords each time new word is instantiated', () => {
     expect(trie.count()).to.equal(0);
     trie.insert('poop')
     expect(trie.count()).to.equal(1);
+  });
+
+  it('should insert word when calling insert', () => {
+    trie.insert('help')
+    trie.insert('hello')
+    console.log(JSON.stringify(trie, null, 4))
+    expect(trie.root.children.letter).to.equal('h')
   });
 
 })
