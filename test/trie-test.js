fb984019a398a736f457247ae178fcf0b79bf395
bhayek12@gmail.com <Ben@Bens-MacBook-Pro-2.local>
2018-08-29 15:04:21 -0600

    Inserted dictionary

--- test/trie-test.js
+++ test/trie-test.js
@@ -1,9 +1,12 @@
 import { expect } from'chai';
 import Trie from '../lib/trie';
 import Node from '../lib/Node';
+import fs from 'fs';
 
 describe('TRIE', () => {
   let trie;
+  const text = "/usr/share/dict/words";
+  const dictionary = fs.readFileSync(text).toString().trim().split('\n');
 
   beforeEach( () => {
     trie = new Trie();
@@ -39,8 +42,8 @@ describe('TRIE', () => {
   it('should suggest as array of words', () => {
     trie.insert('hello');
     trie.insert('help');
-    trie.insert('poop');
-    let response = trie.suggest('h');
+
+    let response = trie.suggest('he');
     console.log(JSON.stringify(trie, null, 4))
     expect(response).to.deep.eq(['hello', 'help'])
   });
